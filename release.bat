60cede05ee799981c8aca9e93ab4e06dbf90ae30
Effekseer <effekseer@gmail.com>
2019-01-03 19:56:36 +0900

    Added release bat

---
+++ release.bat
@@ -0,0 +1,9 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" Dev\EffekseerForDXLib_vs2015.sln /p:configuration=Debug /p:platform=win32
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" Dev\EffekseerForDXLib_vs2015.sln /p:configuration=Release /p:platform=win32
+
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" Dev\EffekseerForDXLib_vs2015.sln /p:configuration=Debug /p:platform=x64
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" Dev\EffekseerForDXLib_vs2015.sln /p:configuration=Release /p:platform=x64
+
+python generate_release_package.py
+
+pause
