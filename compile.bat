6a689aa8aec369899ea36fa4d1206d36f2f94985
Effekseer <effekseer@gmail.com>
2019-01-03 23:30:25 +0900

    remove a file

--- compile.bat
+++
@@ -1,8 +0,0 @@
-
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" Dev\EffekseerForDXLib_vs2015.sln /p:configuration=Debug /p:platform=win32
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" Dev\EffekseerForDXLib_vs2015.sln /p:configuration=Release /p:platform=win32
-
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" Dev\EffekseerForDXLib_vs2015.sln /p:configuration=Debug /p:platform=x64
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" Dev\EffekseerForDXLib_vs2015.sln /p:configuration=Release /p:platform=x64
-
-pause
