95e3f9c155f769bb654d94f8890810f93103701a
durswd <swda.durl@gmail.com>
2021-03-09 00:50:03 +0900

    Revert "Update a label"
    This reverts commit 84f0a3c79e7602ebfc41064219c58052a33a4458.

--- generate_release_package.py
+++ generate_release_package.py
@@ -2,7 +2,7 @@
 
 import utils
 
-targetDir = 'EffekseerForDXLib_153c_322c/'
+targetDir = 'EffekseerForDXLib_153a_322a/'
 dxlibDir = 'DXLib_VC/'
 effekseerDir = '../Effekseer/'
 effekseerVSDir = effekseerDir + 'EffekseerRuntime150/Compiled/'
