ffbe3df0ab5da764816db9b79c028910bc545f59
durswd <swda.durl@gmail.com>
2022-06-13 21:45:19 +0900

    1.62e

--- generate_release_package.py
+++ generate_release_package.py
@@ -2,7 +2,7 @@
 
 import utils
 
-targetDir = 'EffekseerForDXLib_161c_322c/'
+targetDir = 'EffekseerForDXLib_162e_323/'
 dxlibDir = 'DXLib_VC/'
 effekseerDir = '../Effekseer/'
 effekseerVSDir = effekseerDir + 'EffekseerRuntime_DXLib/Compiled/'
